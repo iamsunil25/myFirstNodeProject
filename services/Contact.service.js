12dd4af8d657866351b50ec66838cfe966d3c021
Sunil Joshi <suniljoshi2621@gmail.com>
2022-11-27 00:19:42 +0530

    debug code added

--- services/Contact.service.js
+++ services/Contact.service.js
@@ -4,13 +4,14 @@ const { sendEmail } = require("./email.service");
 // add contact-us details service
 const addContactUsDetailsService = async (contactDetails)=>{
 	try {
-	let {contact_number, email} = contactDetails; 
-	let isConatctUsDetailsAlreadyPresent = await ContactUsModel.find({$or: [ {contactNumber:{$eq: contact_number}}, {email:{$eq: email}} ] }) 
+		console.log("req body",contactDetails);
+
+	let isConatctUsDetailsAlreadyPresent = await ContactUsModel.find({$or: [ {contactNumber:{$eq: contactDetails.contact_number}}, {email:{$eq: contactDetails.email}} ] }) 
+	console.log("🚀 ~ file: Contact.service.js ~ line 10 ~ addContactUsDetailsService ~ isConatctUsDetailsAlreadyPresent", isConatctUsDetailsAlreadyPresent)
 	
-	console.log("req body",isConatctUsDetailsAlreadyPresent);
 	
 	if(isConatctUsDetailsAlreadyPresent.length===0){
-		console.log("filled data", isConatctUsDetailsAlreadyPresent[0]);
+		console.log("filled data", isConatctUsDetailsAlreadyPresent);
 	 throw new Error("You have already filled the form")
     }
 
