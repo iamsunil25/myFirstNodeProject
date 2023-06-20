acf05ba5acf99b744260a31cf2c8c7982efc5a23
Sunil Joshi <suniljoshi2621@gmail.com>
2023-06-21 01:28:15 +0530

    v1 added in url

--- routes/Portfolio.route.js
+++ routes/Portfolio.route.js
@@ -6,7 +6,7 @@ const { WELCOME_MESSAGE } = require("../constantMessage");
 
 const router = express.Router();
  
-router.route("/contact-us").post(addContactUsDetailsController)
+router.route("/v1/contact-us").post(addContactUsDetailsController)
 // router.route("/welcome").get(async(_,res)=>{
 // 	res.status(200).json({message: WELCOME_MESSAGE})
 // })
