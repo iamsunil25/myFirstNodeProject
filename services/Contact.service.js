bb8a76124fc0fa5a405520dbf1c3125ee5f22f4d
Sunil Joshi <suniljoshi2621@gmail.com>
2024-01-19 00:04:47 +0530

    mongo stopped

--- services/Contact.service.js
+++ services/Contact.service.js
@@ -4,11 +4,11 @@ const { sendEmail } = require("./email.service");
 // add contact-us details service
 const addContactUsDetailsService = async (contactDetails)=>{
 	try {
-		let addContactData = await ContactUsModel.create(contactDetails);
-		if(addContactData){
+		// let addContactData = await ContactUsModel.create(contactDetails);
+		if(true){
 	 sendEmail(contactDetails)
 		}
-return addContactData;
+return {success:true};
 	} catch (error) {
 // 		console.log("error while storing contact details", error);
 // 		if(error.code===11000){	
