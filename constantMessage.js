5d872bc1068a75e6e6c0a62ca7323ab90f0c546d
Sunil Joshi <suniljoshi2621@gmail.com>
2024-01-19 00:20:06 +0530

    test msg added

--- constantMessage.js
+++ constantMessage.js
@@ -2,7 +2,7 @@
  const MONGODB_CONNECTION_SUCCEED = "Connected to mongodb"
  const SERVER_LISTENING_MESSAGE = "Server Listening at"
  const ALREADY_RAISED_QUERY = "You have already raised a query with this data."
- const QUERY_RAISED_SUCCESSFULL= "Your query raised Successfully"
+ const QUERY_RAISED_SUCCESSFULL= "Your query raised Successfully gracias."
  const WELCOME_MESSAGE="Hurray! Server is listening...";
  module.exports={
 	MONGODB_CONNECTION_ERROR,
